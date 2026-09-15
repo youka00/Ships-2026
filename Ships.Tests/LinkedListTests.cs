d9bdbe5d777ee94f4f4a54ca7577609222e8accc
Youka <112777620+youka00@users.noreply.github.com>
2026-09-15 05:34:37 +0300

    This commit is for the unit test. IT also kinda broke after i insalled the new version of .net. It didnt give any errosr but the code was yellow (it gave me warnings) so i tried to fix them)

--- Ships.Tests/LinkedListTests.cs
+++ Ships.Tests/LinkedListTests.cs
@@ -13,4 +13,34 @@ public class LinkedListTests
 
 		Assert.Equal(3, list.Count);
 	}
+
+	[Fact]
+	public void TestRemove()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+
+		list.Add(1);
+		list.Add(6);
+		list.Add(-1);
+
+		Assert.True(list.Remove(6));
+		Assert.Equal(2, list.Count);
+		Assert.DoesNotContain(6, list);
+	}
+	
+	[Fact]
+	public void TestContains()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+
+		list.Add(1);
+		list.Add(6);
+		list.Add(-1);
+
+		Assert.True(list.Contains(6));
+		Assert.DoesNotContain(10, list);
+		// after installin the new version of .Net my Assert.False did not work. it did work while i used .net 8.0
+		// Changed Assert.False to Assert.DoesNotContain with microsoft learns help
+		// I couldn't find any solution for Assert.True and honestly i dont know why this did happen.
+	}
 }
